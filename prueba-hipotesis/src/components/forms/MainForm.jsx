3759b403089009d76a532fc2b955ce03b4eeb2eb
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-04-21 00:37:01 -0600

    format: Delete Unused code

--- prueba-hipotesis/src/components/forms/MainForm.jsx
+++ prueba-hipotesis/src/components/forms/MainForm.jsx
@@ -19,15 +19,6 @@ function MainForm () {
 
   const handleSubmit = async (ev) => {
     ev.preventDefault()
-    console.log(formData)
-    const params = {
-      mean_sample: 497.3,
-      mean_population: 500,
-      dev_stand: 5,
-      n: 100,
-      ns: 0.05
-    }
-
     console.log(params)
     try {
       const response = await axios.get('http://127.0.0.1:8000/mean/know_dev_estand/', {
