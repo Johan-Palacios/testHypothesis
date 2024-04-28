46a5613824edd7d8cc55887b24e5b5b22d86418f
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-04-28 00:17:30 -0600

    feat: Delete unused code

--- prueba-hipotesis/src/components/Conclusion.jsx
+++ prueba-hipotesis/src/components/Conclusion.jsx
@@ -5,17 +5,6 @@ import HipotesisConclusionContext from '../context/hipotesisConcusionContext'
 const Conclusion = () => {
   const { hipotesisConclusion } = useContext(HipotesisConclusionContext)
   const [conclusion, setConclusion] = useState({})
-  const [typeAnalisis, setTypeAnalisis] = useState(0)
-  const handleChange = (ev) => {
-    ev.preventDefault()
-    const value = ev.target.value
-    if (value === '') {
-      setTypeAnalisis(0)
-    } else {
-      setTypeAnalisis(ev.target.value)
-    }
-  }
-
   useEffect(() => {
     setConclusion({ ...hipotesisConclusion }
     )
@@ -41,14 +30,6 @@ const Conclusion = () => {
               </Text>
 
             </Text>
-
-            <Select placeholder='Seleccione un tipo de analisis' onChange={(ev) => handleChange(ev)}>
-              <option value={1}>&lt;</option>
-              <option value={2}>&gt;</option>
-              <option value={3}>!=</option>
-            </Select>
-
-            {typeAnalisis === 0 ? <></> : <Input placeholder='Nombre de la variable' />}
 
           </Stack>
         </CardBody>
