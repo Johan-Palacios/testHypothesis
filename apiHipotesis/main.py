dbc37ea90a5cb0f7e407b6f3d1dbecf1eb96f577
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-05-06 09:49:49 -0600

    format: delete / endpoint

--- apiHipotesis/main.py
+++ apiHipotesis/main.py
@@ -18,11 +18,6 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 # Mean Cases
 @app.get("/mean/know_dev_estand/")
 def read_mean_know_dev_estand(mean_sample: float, mean_population: float, dev_stand: float, n: int, ns: float):
