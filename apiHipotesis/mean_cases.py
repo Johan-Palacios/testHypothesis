c01c713fc869b8d5861daf762cf4ec5f11dfb013
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-05-06 09:49:30 -0600

    feat: add mean_unkonw_dev_stand_t endpoint in api

--- apiHipotesis/mean_cases.py
+++ apiHipotesis/mean_cases.py
@@ -7,3 +7,7 @@ def mean_know_dev_estand(mean_sample: float, mean_population: float, dev_estand:
     est_p:float = float (mean_sample - mean_population) / (dev_estand / float(sqrt(n)))
     return {"criticPoint": z, "observedValue": est_p}
 
+def mean_unknown_dev_stand_t(mean_sample: float, mean_population: float, sample_dev_estand: float, n: float, ns: float):
+    z = float(t.ppf(ns, n-1))
+    est_p:float = float (mean_sample - mean_population) / (sample_dev_estand / float(sqrt(n)))
+    return {"criticPoint": z, "observedValue": est_p}
