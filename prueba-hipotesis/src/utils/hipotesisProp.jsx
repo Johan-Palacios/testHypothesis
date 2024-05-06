396a21e6d4c8d0e2628b04f32830a75c4a766be3
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-05-06 09:48:46 -0600

    feat: add unknow_dev_stand_t/ case for mean

--- prueba-hipotesis/src/utils/hipotesisProp.jsx
+++ prueba-hipotesis/src/utils/hipotesisProp.jsx
@@ -9,7 +9,7 @@ const testHipotesis = {
           caseDefinition: 'x ~ N(μ, σ)',
           image: 'mean_know_dev_stand.png',
           statisticsTest: '',
-          endpoint: 'know_dev_estand',
+          endpoint: 'mean/know_dev_estand/',
           reqData: {
             mean_sample: {
               name: 'Media Muestral',
@@ -35,7 +35,41 @@ const testHipotesis = {
               placeholder: 'Ingrese la Muestra'
             }
           }
+        },
+
+        {
+          name: 'Deviación Estándar No Conocida t',
+          caseDefinition: 'x ~ N(μ, σ)',
+          image: 'mean_know_dev_stand.png',
+          statisticsTest: '',
+          endpoint: 'mean/unknown_dev_stand_t/',
+          reqData: {
+            mean_sample: {
+              name: 'Media Muestral',
+              type: 'number',
+              placeholder: 'Ingrese la Media Muestral Ej. 497.3',
+              step: 0.1
+            },
+            mean_population: {
+              name: 'Media Poblacional',
+              type: 'number',
+              maininterest: 1,
+              placeholder: 'Ingrese la Media Poblacional Ej. 500'
+            },
+            sample_dev_estand:
+            {
+              name: 'Desviación Estandar Muestral',
+              type: 'number',
+              placeholder: 'Ingrese la Desvacíón Standard'
+            },
+            n: {
+              name: 'Muestra',
+              type: 'number',
+              placeholder: 'Ingrese la Muestra'
+            }
+          }
         }
+
       ]
     },
     {
