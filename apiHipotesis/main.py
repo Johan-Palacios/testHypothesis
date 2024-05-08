0faa4592fa232a03fed66e063e948c6b5bfc5315
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-05-08 00:19:23 -0600

    typo: rewrite estand to stand

--- apiHipotesis/main.py
+++ apiHipotesis/main.py
@@ -19,21 +19,21 @@ app.add_middleware(
 
 
 # Mean Cases
-@app.get("/mean/know_dev_estand/")
-def read_mean_know_dev_estand(
+@app.get("/mean/know_dev_stand/")
+def read_mean_know_dev_stand(
     mean_sample: float, mean_population: float, dev_stand: float, n: int, ns: float
 ):
-    return mean_know_dev_estand(mean_sample, mean_population, dev_stand, n, ns)
+    return mean_know_dev_stand(mean_sample, mean_population, dev_stand, n, ns)
 
 
 @app.get("/mean/unknown_dev_stand_t/")
 def read_mean_unknown_dev_stand_t(
     mean_sample: float,
     mean_population: float,
-    sample_dev_estand: float,
+    sample_dev_stand: float,
     n: int,
     ns: float,
 ):
     return mean_unknown_dev_stand_t(
-        mean_sample, mean_population, sample_dev_estand, n, ns
+        mean_sample, mean_population, sample_dev_stand, n, ns
     )
--- apiHipotesis/mean_cases.py
+++ apiHipotesis/mean_cases.py
@@ -2,12 +2,12 @@ from scipy.stats import norm, t
 from math import sqrt
 
 
-def mean_know_dev_estand(mean_sample: float, mean_population: float, dev_estand: float, n: float, ns: float):
+def mean_know_dev_stand(mean_sample: float, mean_population: float, dev_stand: float, n: float, ns: float):
     z = float(norm.ppf(ns))
-    est_p:float = float (mean_sample - mean_population) / (dev_estand / float(sqrt(n)))
+    est_p:float = float (mean_sample - mean_population) / (dev_stand / float(sqrt(n)))
     return {"criticPoint": z, "observedValue": est_p}
 
-def mean_unknown_dev_stand_t(mean_sample: float, mean_population: float, sample_dev_estand: float, n: float, ns: float):
+def mean_unknown_dev_stand_t(mean_sample: float, mean_population: float, sample_dev_stand: float, n: float, ns: float):
     z = float(t.ppf(ns, n-1))
-    est_p:float = float (mean_sample - mean_population) / (sample_dev_estand / float(sqrt(n)))
+    est_p:float = float (mean_sample - mean_population) / (sample_dev_stand / float(sqrt(n)))
     return {"criticPoint": z, "observedValue": est_p}
