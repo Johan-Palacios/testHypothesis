da195fbbb1091b9208a6becea28db2fb6be356f7
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-05-12 15:36:49 -0600

    format: Format mean cases and proportion cases to black style

--- apiHipotesis/mean_cases.py
+++ apiHipotesis/mean_cases.py
@@ -2,12 +2,27 @@ from scipy.stats import norm, t
 from math import sqrt
 
 
-def mean_know_dev_stand(mean_sample: float, mean_population: float, dev_stand: float, n: float, ns: float):
+def mean_know_dev_stand(
+    mean_sample: float,
+    mean_population: float,
+    dev_stand: float,
+    n: float,
+    ns: float,
+):
     z = float(norm.ppf(ns))
-    est_p:float = float (mean_sample - mean_population) / (dev_stand / float(sqrt(n)))
+    est_p: float = float(mean_sample - mean_population) / (dev_stand / float(sqrt(n)))
     return {"criticPoint": z, "observedValue": est_p}
 
-def mean_unknown_dev_stand_t(mean_sample: float, mean_population: float, sample_dev_stand: float, n: float, ns: float):
-    z = float(t.ppf(ns, n-1))
-    est_p:float = float (mean_sample - mean_population) / (sample_dev_stand / float(sqrt(n)))
+
+def mean_unknown_dev_stand_t(
+    mean_sample: float,
+    mean_population: float,
+    sample_dev_stand: float,
+    n: float,
+    ns: float,
+):
+    z = float(t.ppf(ns, n - 1))
+    est_p: float = float(mean_sample - mean_population) / (
+        sample_dev_stand / float(sqrt(n))
+    )
     return {"criticPoint": z, "observedValue": est_p}
--- apiHipotesis/proportion_cases.py
+++ apiHipotesis/proportion_cases.py
@@ -2,9 +2,11 @@ from scipy.stats import norm
 from math import sqrt
 
 
-def proportion_bernoulli_n(proportion_population: float, proportion_sample: float, ns:float, n:float):
+def proportion_bernoulli_n(
+    proportion_population: float, proportion_sample: float, ns: float, n: float
+):
     z = abs(float(norm.ppf(ns)))
-    est_p = (proportion_sample - proportion_population)/(sqrt((proportion_population *(1 - proportion_population))/n))
+    est_p = (proportion_sample - proportion_population) / (
+        sqrt((proportion_population * (1 - proportion_population)) / n)
+    )
     return {"criticPoint": z, "observedValue": est_p}
-
-
