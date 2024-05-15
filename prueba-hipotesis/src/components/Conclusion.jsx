9ce9ceb890cd7d98c414a4834d877aec9ca3884d
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-05-15 02:02:00 -0600

    feat: conclusion improve ui and conclusion generation

--- prueba-hipotesis/src/components/Conclusion.jsx
+++ prueba-hipotesis/src/components/Conclusion.jsx
@@ -1,4 +1,5 @@
-import { Container, Text, Card, CardBody, CardHeader, Heading, Stack, Image } from '@chakra-ui/react'
+import { Container, Text, Card, CardBody, CardHeader, Heading, Stack, Image, Alert, IconButton } from '@chakra-ui/react'
+import { CopyIcon } from '@chakra-ui/icons'
 import axios from 'axios'
 import { useContext, useEffect, useState } from 'react'
 import HipotesisConclusionContext from '@context/hipotesisConcusionContext'
@@ -13,6 +14,8 @@ const Conclusion = () => {
   const [alternativeHipotesis, setAlternativeHipotesis] = useState(false)
   const [graph, setGraph] = useState('')
   const [params, setParams] = useState({})
+  const [ns, setNs] = useState(0)
+  const [textConclusion, setTextConclusion] = useState('')
 
   useEffect(() => {
     setConclusion({ ...hipotesisConclusion }
@@ -61,20 +64,39 @@ const Conclusion = () => {
     if (Object.keys(params).length !== 0 &&
       params.observedValue !== undefined &&
       params.criticPoint !== undefined &&
-      params.analisisType !== undefined) {
+      params.analisisType !== undefined &&
+    hipotesisDefinition.inputdata.n) {
       fetchGraphData()
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params])
+  useEffect(() => {
+    setNs(hipotesisConclusion.ns)
+  }, [hipotesisConclusion.ns])
 
   useEffect(() => {
     setParams({
       observedValue: conclusion.observedValue,
       criticPoint: conclusion.criticPoint,
-      analisisType: conclusion.analisisType
+      analisisType: conclusion.analisisType,
+      n: hipotesisDefinition.inputdata.n
     })
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [conclusion.observedValue, conclusion.criticPoint, conclusion.analisisType])
+  }, [conclusion.observedValue, conclusion.criticPoint, conclusion.analisisType, hipotesisDefinition.inputdata.n])
+
+  useEffect(() => {
+    if (alternativeHipotesis && ns !== undefined) {
+      setTextConclusion(
+        `Con un nivel de significancia de ${ns} existe evidencia estadisticamente significativa
+          para rechazar la Hipotesis Nula y aceptar la Hipotesis Alternativa`
+      )
+    } else if (alternativeHipotesis === false && ns !== undefined) {
+      setTextConclusion(
+        `Con un nivel de significancia de ${ns} existe evidencia estadisticamente significativa
+        para rechazar la Hipotesis Alternativa y aceptar la hipotesis Nula`
+      )
+    }
+  }, [alternativeHipotesis, ns])
 
   return (
     <Container marginBottom={16}>
@@ -86,19 +108,31 @@ const Conclusion = () => {
           <Stack>
             <Heading as='h3'>Hipotesis</Heading>
             <Hipotesis />
-            <Text>Valor Critico:&nbsp;
+            <Alert status='success' display='flex' flexDirection='row' gap={6} justifyContent='space-between'>
+
+              <IconButton icon={<CopyIcon />} onClick={() => { navigator.clipboard.writeText(criticPointValue) }} />
+              Valor Critico:&nbsp;
               <Text as='b'>
                 {criticPointValue}
               </Text>
-            </Text>
-            <Text>El valor observado es:&nbsp;
+            </Alert>
+            <Alert status='warning' display='flex' flexDirection='row' gap={6} justifyContent='space-between'>
+
+              <IconButton icon={<CopyIcon />} onClick={() => { navigator.clipboard.writeText(conclusion.observedValue) }} />
+              El valor observado es:&nbsp;
               <Text as='b'>
                 {conclusion.observedValue}
+
               </Text>
-            </Text>
-            {alternativeHipotesis
-              ? <Text>Se rechaza la Hipotesis Nula y Se acepta la Hipotesis Alternativa</Text>
-              : <Text>Se rechaza la Hipotesis Alternativa y se acepta la hipotesis Nula</Text>}
+            </Alert>
+            <Alert status='info' display='flex' flexDirection='row' gap={6} justifyContent='space-between'>
+              <IconButton
+                icon={<CopyIcon />} onClick={() => {
+                  navigator.clipboard.writeText(textConclusion)
+                }}
+              />
+              {textConclusion}
+            </Alert>
             <Text />
             {graph.graph !== undefined ? <Image src={`data:image/png;base64, ${graph.graph}`} /> : <></>}
 
