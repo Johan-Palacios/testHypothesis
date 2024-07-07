4ac7f027e1f57066bac6db8bf3cd8644e6614371
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-07-07 00:28:16 -0600

    feat: Production Environment

--- prueba-hipotesis/vite.config.js
+++ prueba-hipotesis/vite.config.js
@@ -13,5 +13,17 @@ export default defineConfig({
       '@utils': path.resolve(__dirname, './src/utils'),
       '@hooks': path.resolve(__dirname, './src/hooks')
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks (id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    }
   }
+
 })
